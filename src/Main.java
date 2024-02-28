49ac04632df7b93aa4f08e50e4abec9445460fee
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-02-28 08:42:00 -0300

    Adicionando classe principal

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,25 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Aluno aluno = new Aluno(123, "João", "123.123.123-12",
-                "ADS", 3,
-                LocalDate.of(2000,12,1));
 
-        aluno.setMatricula(963);
+        Professor professor = new Professor(123,
+                "João", "111.111.111-01",
+                LocalDate.of(2000,1,1),
+                2950.50f, "Mestrado");
+        Aluno aluno1 = new Aluno(123, "Maria",
+                "222.222.222-02", "ADS", 3,
+                LocalDate.of(2001,5,3));
+        Aluno aluno2 = new Aluno(321, "Pedro",
+                "333.333.333-03", "ADS", 3,
+                LocalDate.of(2001,5,3));
+        Aluno aluno3 = new Aluno(852, "José",
+                "444.444.444-04", "ADS", 3,
+                LocalDate.of(2001,5,3));
 
-        aluno.setNome("João da Silva");
+        Turma turma = new Turma(professor, aluno1, aluno2, aluno3,
+                "POO");
 
-        System.out.println(aluno.getNome().toUpperCase());
-
+        System.out.println(turma.getProfessor().getNome());
 
     }
 }
