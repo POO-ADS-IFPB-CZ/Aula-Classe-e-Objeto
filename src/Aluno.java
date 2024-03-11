fe13fc8e3cef17c5e79a1df269a3e573fa593d0c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-03-11 08:04:31 -0300

    Adicioando herança entre aluno e pessoa

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,39 +1,13 @@
 import java.time.LocalDate;
-public class Aluno {
-    private int matricula;
-    private String nome;
-    private String cpf;
+public class Aluno extends Pessoa {
     private String curso;
     private int periodo;
-    private LocalDate nascimento;
 
     public Aluno(int matricula, String nome, String cpf, String curso,
                  int periodo, LocalDate nascimento){
-        this.matricula = matricula;
-        this.nome = nome;
-        this.cpf = cpf;
+        super(nome, cpf, nascimento, matricula);
         this.curso = curso;
         this.periodo = periodo;
-        this.nascimento = nascimento;
-    }
-
-    public Aluno(int matricula){
-        this.matricula = matricula;
-    }
-
-    public Aluno(){
-    }
-
-    public int getMatricula(){
-        return matricula;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public String getCpf(){
-        return cpf;
     }
 
     public String getCurso(){
@@ -44,21 +18,6 @@ public class Aluno {
         return periodo;
     }
 
-    public LocalDate getNascimento(){
-        return nascimento;
-    }
-
-    public void setMatricula(int matricula){
-        this.matricula = matricula;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public void setCpf(String cpf){
-        this.cpf = cpf;
-    }
 
     public void setCurso(String curso){
         this.curso = curso;
@@ -66,10 +25,6 @@ public class Aluno {
 
     public void setPeriodo(int periodo){
         this.periodo = periodo;
-    }
-
-    public void setNascimento(LocalDate nascimento){
-        this.nascimento = nascimento;
     }
 
 }
